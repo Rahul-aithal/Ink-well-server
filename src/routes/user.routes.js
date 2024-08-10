646972fdc7227cb94b48bf548cd2d27f75e6f6e7
Rahul-aithal <aithalrahul34@gmail.com>
2024-08-10 12:53:45 +0530

    route to search by username

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import { changeCurrentPassword, getCurrentUser, getstoryHistory, refreshAccessToken, signIn, signOut, signUp, updateEmail, updateUsername} from "../controllers/user.controller.js";
+import { changeCurrentPassword, getCurrentUser, getstoryHistory, refreshAccessToken, searchUserByUserName, signIn, signOut, signUp, updateEmail, updateUsername} from "../controllers/user.controller.js";
 import { verifyToken } from "../middlewares/verifyJWT.middleware.js";
 
 const router=Router();
@@ -15,6 +15,7 @@ router.put("/update-username",verifyToken,updateUsername);
 router.put("/update-email",verifyToken,updateEmail);
 router.get("/get-user",verifyToken,getCurrentUser);
 router.get("/get-user-history",verifyToken,getstoryHistory);
+router.get("/get-user-by-username",verifyToken,searchUserByUserName);
 
 
 export default router
