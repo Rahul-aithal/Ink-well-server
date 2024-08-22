6cadc1c9473eaf24e072519dbf45bf9ae67d6892
Rahul-aithal <aithalrahul34@gmail.com>
2024-08-22 22:46:49 +0530

    Story from Sotry

--- src/models/comment.models.js
+++ src/models/comment.models.js
@@ -1,21 +1,20 @@
 import mongoose, { Schema } from "mongoose";
 
-const commentSchema = new mongoose.Schema({
-    comment:{
-        type:String,
-        required:[true,"Comments are requried"]
+const commentSchema = new Schema({
+    comment: {
+        type: String,
+        required: [true, "Comments are requried"],
     },
-    commentedStory:{
-        type:Schema.Types.ObjectId,
-        ref:"Stroy",
-        required:[true,"Story id is requried for comments"]
+    commentedStory: {
+        type: Schema.Types.ObjectId,
+        ref: "Story",
+        required: [true, "Story id is requried for comments"],
     },
-    commenter:{
-        type:Schema.Types.ObjectId,
-        ref:"User",
-        required:[true,"Commenter's id is requried for comments"]
-    }
-})
+    commenter: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: [true, "Commenter's id is requried for comments"],
+    },
+});
 
-
-export const Comment=mongoose.model("Comment",commentSchema)
+export const Comment = mongoose.model("Comment", commentSchema);
--- src/models/like.models.js
+++ src/models/like.models.js
@@ -1,17 +1,16 @@
 import mongoose, { Schema } from "mongoose";
 
-const likeSchema = new mongoose.Schema({
-    likedStory:{
-        type:Schema.Types.ObjectId,
-        ref:"Stroy",
-        required:[true,"Story id is requried for comments"]
+const likeSchema = new Schema({
+    likedStory: {
+        type: Schema.Types.ObjectId,
+        ref: "Story",
+        required: [true, "Story id is requried for comments"],
     },
-    likedUser:{
-        type:Schema.Types.ObjectId,
-        ref:"User",
-        required:[true,"Commenter's id is requried for comments"]
-    }
-})
+    likedUser: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: [true, "Commenter's id is requried for comments"],
+    },
+});
 
-
-export const Like=mongoose.model("Like",likeSchema)
+export const Like = mongoose.model("Like", likeSchema);
