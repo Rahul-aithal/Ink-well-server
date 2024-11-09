c1acd2c0af17ef9598ca5244c964a21caf2ad579
Rahul-aithal <aithalrahul34@gmail.com>
2024-11-09 09:30:58 +0530

    added process env

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,7 @@ import { DB_NAME } from "../constants.js";
 
 const ConnnectDb=async()=>{
 try {
-    const connectionIstance = await mongoose.connect(`mongodb://localhost:27017/${DB_NAME}`);
+    const connectionIstance = await mongoose.connect(`${process.env.DB_URL}${DB_NAME}`);
     console.log("Mongo DB connected at host || DB Host:: ",connectionIstance.connection.host);
     
 } catch (error) {
