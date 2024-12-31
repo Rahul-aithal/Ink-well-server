941fd67bbedf7e4865e3dacea7d4ee72abaa5a7a
Rahul-aithal <aithalrahul34@gmail.com>
2024-12-31 20:13:57 +0530

    added start script to package.json and updated MongoDB connection string with retryWrites option

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,7 @@ import { DB_NAME } from "../constants.js";
 
 const ConnnectDb=async()=>{
 try {
-    const connectionIstance = await mongoose.connect(`${process.env.DB_URL}${DB_NAME}`);
+    const connectionIstance = await mongoose.connect(`${process.env.DB_URL}${DB_NAME}?retryWrites=true&w=majority`);
     console.log("Mongo DB connected at host || DB Host:: ",connectionIstance.connection.host);
     
 } catch (error) {
