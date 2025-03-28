3760d3b2c9a475d5a1031e2b2b085ab2cd9b8a4e
Rahul-aithal <aithalrahul34@gmail.com>
2025-03-28 19:03:33 +0530

    refactor: clean up app.js formatting and add health check HTML page

--- src/app.js
+++ src/app.js
@@ -2,30 +2,27 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
+const app = express();
 
-const app=express();
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGN,
+        credentials: true,
+    })
+);
 
-app.use(cors({
-    origin:process.env.CORS_ORIGN,
-    credentials:true,
-    
-}));
-
-app.use(express.json({limit:"16kb"}));
+app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: false, limit: "16kb" }));
-app.use(express.static("public"))
+app.use(express.static("public"));
 app.use(cookieParser());
 
 //Routes
 
-import userRouter from './routes/user.routes.js'
-import storyRouter from './routes/story.routes.js'
-
+import userRouter from "./routes/user.routes.js";
+import storyRouter from "./routes/story.routes.js";
 
 //Routes declartion
-app.use('/api',userRouter);
-app.use('/api',storyRouter);
+app.use("/api", userRouter);
+app.use("/api", storyRouter);
 
-
-
-export {app}
+export { app };
